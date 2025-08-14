ae780a90cf4cb95b3deba716a371baab39453a63
Sreenuanum45 <anumandlasreenu@gmail.com>
2025-08-14 21:06:15 +0530

    Sreenu1 .java Changes done

--- src/main/java/Sreenu1.java
+++ src/main/java/Sreenu1.java
@@ -1,5 +1,6 @@
 public class Sreenu1 {
     public static void main(String[] args) {
         System.out.println("Hello, Sreenu!");
+        System.out.println("Welcome to the Java programming world!");
     }
 }
